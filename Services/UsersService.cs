19560f9cc5c415284554a95110e23c1fbada158c
GektorShaman <zhadan.viktor@gmail.com>
2021-12-03 18:07:07 +0300

    Add base for work with user

---
+++ Services/UsersService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SevsuFacilityStorage.Abstractions;
+using SevsuFacilityStorage.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SevsuFacilityStorage.Services
+{
+    public class UsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+       private readonly RoleManager<IdentityRole> _roleManager;
+
+       private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersService(IUsersRepository usersRepository, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _usersRepository = usersRepository;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task GetAllUsersAsync()
+        {
+            var allUsers = _usersRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach (var user in allUsers)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                UserViewModel model = new UserViewModel
+                {
+                    Name = user.UserName,
+                    Role = userRoles.FirstOrDefault()
+                };
+                result.Add(model);
+            }
+        }
+
+    }
+}
